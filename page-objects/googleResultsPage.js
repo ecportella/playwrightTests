1487cfbb751c998c9372c25c54f09a6719c8d5ff
Eugenio Portella <eugenio.portella@toptal.com>
2023-08-14 10:53:53 -0300

    Add Page Objects

---
+++ page-objects/googleResultsPage.js
@@ -0,0 +1,18 @@
+import { platform } from "os";
+
+// SELECTORS
+const SEARCH_RESULTS = "#search a > h3";
+
+// PAGE OBJECT ACTIONS
+export const openResultByIndex = async (page, index) => {
+  const link = page.locator(SEARCH_RESULTS).nth(index);
+  const [newPage] = await Promise.all([
+    page.context().waitForEvent("page"),
+    link.click({ modifiers: [platform() === "darwin" ? "Meta" : "Control"] }),
+  ]);
+
+  await newPage.bringToFront();
+  await newPage.waitForEvent("load");
+
+  return newPage;
+};
